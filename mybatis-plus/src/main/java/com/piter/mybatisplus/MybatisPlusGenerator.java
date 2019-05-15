eaf11bd104c4eb1881b638ec9d1dedd66ceaa910
Piter <1181720735@qq.com>
2019-05-15 17:33:44 +0800

    demo

--- mybatis-plus/src/main/java/com/piter/mybatisplus/MybatisPlusGenerator.java
+++ mybatis-plus/src/main/java/com/piter/mybatisplus/MybatisPlusGenerator.java
@@ -1,47 +1,69 @@
 package com.piter.mybatisplus;
 
 import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.generator.AutoGenerator;
 import com.baomidou.mybatisplus.generator.InjectionConfig;
-import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+import com.baomidou.mybatisplus.generator.config.FileOutConfig;
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.PackageConfig;
+import com.baomidou.mybatisplus.generator.config.StrategyConfig;
+import com.baomidou.mybatisplus.generator.config.TemplateConfig;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.DateType;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.VelocityTemplateEngine;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
- * @author Piter
+ * mybatis plus 代码生成器(需添加两个依赖)
  */
 public class MybatisPlusGenerator {
 
-    private static final String MUDULE_NAME = "mybatis-plus";
-    private static final String TABLE_NAME = "BANNER";
-    private static final String PARENT_PACKAGE = "com.chinaums.wechatpre";
+    private static final String MUDULE_NAME = "promote-manager";
+    //多张表以逗号分隔,oracle数据库表名必须大写
+    private static final String TABLE_NAME = "PROMOTE_ACTIVITY_INFO,PROMOTE_ACTIVITY_SCOPE";
+    private static final String PARENT_PACKAGE = "com.chinaums.promote.manager";
     private static final String AUTHOR = "Piter";
-
     //jdbc:oracle:thin:@172.16.210.20:1521:zjtsdb
     //jdbc:mysql://localhost:3306/dev?useUnicode=true&characterEncoding=utf8
     public static final String URL = "jdbc:oracle:thin:@172.16.208.13:1521:orcl";
-    //oracle.jdbc.driver.OracleDriver
-    //com.mysql.cj.jdbc.Driver
+    //oracle.jdbc.OracleDriver
+    //com.mysql.jdbc.Driver
     public static final String DRIVER_NAME = "oracle.jdbc.OracleDriver";
     public static final String USER_NAME = "promote_new";
     public static final String PASSWORD = "promote_new2019";
 
+
     public static void main(String[] args) {
+        String moduleName = MUDULE_NAME;
+        String[] tables = TABLE_NAME.split(",");
+
         // 代码生成器
         AutoGenerator mpg = new AutoGenerator();
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        String projectPath = System.getProperty("user.dir") + "/" + MUDULE_NAME;
-        gc.setOpen(false)
-        .setOutputDir(projectPath + "/src/main/java")
-        .setAuthor(AUTHOR)
-        .setServiceName("%sService")
-        .setBaseResultMap(true)
-        .setBaseColumnList(true);
+        String projectPath = System.getProperty("user.dir") + "/" + moduleName;
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor(AUTHOR);
+        gc.setOpen(false);
+        gc.setFileOverride(false); //是否覆盖已有文件
+        gc.setBaseResultMap(true);//生成基本的resultMap
+        gc.setBaseColumnList(true);//生成基本的SQL片段
+        gc.setDateType(DateType.ONLY_DATE);//时间为java.util.Date
+//        gc.setServiceName(null);
+        gc.setServiceImplName("%sService");
+        // gc.setMapperName("%sDao");
+        // gc.setXmlName("%sDao");
+        // gc.setControllerName("%sAction");
+        // gc.setSwagger2(true); 实体属性 Swagger2 注解
+
         mpg.setGlobalConfig(gc);
 
         // 数据源配置
@@ -50,13 +72,77 @@ public class MybatisPlusGenerator {
         dsc.setDriverName(DRIVER_NAME);
         dsc.setUsername(USER_NAME);
         dsc.setPassword(PASSWORD);
+        dsc.setTypeConvert(new OracleTypeConvert());
+        // dsc.setSchemaName("public");
         mpg.setDataSource(dsc);
 
         // 包配置
         PackageConfig pc = new PackageConfig();
         pc.setParent(PARENT_PACKAGE);
+        pc.setServiceImpl("service");
+        pc.setXml(null);
+        pc.setService(null);
+//        pc.setModuleName(scanner("模块名"));
+
         mpg.setPackageInfo(pc);
-        mpg.setTemplate(new TemplateConfig());
+
+
+        // 如果模板引擎是 freemarker
+        //String templatePath = "/templates/mapper.xml.ftl";
+        // 如果模板引擎是 velocity
+         String templatePath = "/templates/mapper.xml.vm";
+
+        // 自定义配置
+        //配置自定义属性注入
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                Map<String, Object> map = new HashMap<>();
+                map.put("autowiredControllerName", "service");
+                map.put("autowiredServiceName", "mapper");
+//                map.put("isPage", isPage);
+                this.setMap(map);
+            }
+        };
+
+        // 自定义输出配置
+        List<FileOutConfig> focList = new ArrayList<>();
+        // 自定义配置会被优先输出
+        focList.add(new FileOutConfig(templatePath) {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输出文件名 ， 如果你 Entity 设置了前后缀、此处注意 xml 的名称会跟着发生变化！！
+                return projectPath + "/src/main/resources/mappers/"
+                        + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+//        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        // templateConfig.setEntity("templates/entity2.java");
+//         templateConfig.setServiceImpl("templates/serviceImpl2.java");
+//         templateConfig.setController("templates/controller2.java");
+
+        //自定义模板名
+        // templateConfig.setEntity("...");
+//        templateConfig.setXml(null);    // 关闭默认 xml 生成，调整生成 至 根目录
+//        templateConfig.setController(null);
+//        templateConfig.setService(null);    //关闭生成service接口
+//        templateConfig.setServiceImpl(null);
+//        templateConfig.setMapper(null);
+        // 如上任何一个模块如果设置 空 OR Null 将不生成该模块。
+        mpg.setTemplate(
+                new TemplateConfig()
+                .setXml(null)
+                .setController(null)
+                .setService(null)
+                .setServiceImpl("templates/serviceImpl3.java")
+        );
 
         // 策略配置
         StrategyConfig strategy = new StrategyConfig();
@@ -64,40 +150,47 @@ public class MybatisPlusGenerator {
         strategy.setColumnNaming(NamingStrategy.underline_to_camel);
         strategy.setEntityLombokModel(true);
         strategy.setRestControllerStyle(true);
-        strategy.setInclude(TABLE_NAME);
+        strategy.setInclude(tables);
         strategy.setSuperEntityColumns("id");
         strategy.setControllerMappingHyphenStyle(true);
         strategy.setTablePrefix(pc.getModuleName() + "_");
+        // strategy.setInclude(new String[] { "user" }); // 需要生成的表
+        // strategy.setExclude(new String[]{"test"}); // 排除生成的表
+        // 自定义实体父类
+        //strategy.setSuperEntityClass("com.baomidou.ant.common.BaseEntity");
+        // 自定义实体，公共字段
+        // strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+        // 自定义 mapper 父类
+        // strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+        // 自定义 service 父类
+        //strategy.setSuperServiceClass("");
+        // 自定义 service 实现类父类
+        // strategy.setSuperServiceImplClass("");
+        // 自定义 controller 父类
+        // strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+        // 【实体】是否生成字段常量（默认 false）
+        // public static final String ID = "test_id";
+        // strategy.setEntityColumnConstant(true);
+        // 【实体】是否为构建者模型（默认 false）
+        // public User setName(String name) {this.name = name; return this;}
+        // strategy.setEntitySerialVersionUID(false);  //是否实现序列化
+
         mpg.setStrategy(strategy);
-
-        // 自定义配置
-        InjectionConfig cfg = new InjectionConfig() {
-            @Override
-            public void initMap() {
-
-            }
-        };
-        List<FileOutConfig> focList = new ArrayList<>();
-        focList.add(new FileOutConfig("/templates/mapper.xml.vm") {
-            @Override
-            public String outputFile(TableInfo tableInfo) {
-                // 自定义输入文件名称
-                return projectPath + "/src/main/resources/mybatis/mapper/"
-                        + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
-            }
-        });
-        cfg.setFileOutConfigList(focList);
-        mpg.setCfg(cfg);
-        //关闭默认xml 生成
-        mpg.setTemplate(
-                new TemplateConfig()
-                    .setXml(null)
-                    .setController(null)
-                    .setService(null)
-                    .setServiceImpl(null)
-        );
-
-        //执行
+        mpg.setTemplateEngine(new VelocityTemplateEngine());
         mpg.execute();
     }
+
+    /**
+     * 首字母转小写
+     * @param name
+     * @return
+     */
+    public static String getAutowiredName(String name){
+        if(!StringUtils.isEmpty(name)) {
+            return (new StringBuilder()).append(Character.toLowerCase(name.charAt(0))).append(name.substring(1)).toString();
+        }else {
+            return "";
+        }
+    }
+
 }
---
+++ mybatis-plus/src/main/java/com/piter/mybatisplus/OracleTypeConvert.java
@@ -0,0 +1,49 @@
+package com.piter.mybatisplus;
+
+import com.baomidou.mybatisplus.generator.config.GlobalConfig;
+import com.baomidou.mybatisplus.generator.config.ITypeConvert;
+import com.baomidou.mybatisplus.generator.config.rules.DbColumnType;
+import com.baomidou.mybatisplus.generator.config.rules.IColumnType;
+
+/**
+ * ORACLE 字段类型转换
+ * @author lyq
+ * @time 2019/5/4 17:42
+ */
+public class OracleTypeConvert implements ITypeConvert {
+    @Override
+    public IColumnType processTypeConvert(GlobalConfig globalConfig, String fieldType) {
+        String t = fieldType.toLowerCase();
+        if (t.contains("char")) {
+            return DbColumnType.STRING;
+        } else if (t.contains("date") || t.contains("timestamp")) {
+            switch (globalConfig.getDateType()) {
+                case ONLY_DATE:
+                    return DbColumnType.DATE;
+                case SQL_PACK:
+                    return DbColumnType.TIMESTAMP;
+                case TIME_PACK:
+                    return DbColumnType.LOCAL_DATE_TIME;
+            }
+        } else if (t.contains("number")) {
+            if (t.matches("number\\(+\\d\\)")) {
+                return DbColumnType.INTEGER;
+            } else if (t.matches("number\\(+\\d{2}+\\)")) {
+                return DbColumnType.LONG;
+            }
+            return DbColumnType.BIG_DECIMAL;
+        } else if (t.contains("float")) {
+            return DbColumnType.FLOAT;
+        } else if (t.contains("clob")) {
+            return DbColumnType.STRING;
+        } else if (t.contains("blob")) {
+            return DbColumnType.BLOB;
+        } else if (t.contains("binary")) {
+            return DbColumnType.BYTE_ARRAY;
+        } else if (t.contains("raw")) {
+            return DbColumnType.BYTE_ARRAY;
+        }
+        return DbColumnType.STRING;
+    }
+
+}
