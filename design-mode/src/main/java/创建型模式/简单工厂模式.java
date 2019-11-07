a0e4339367be7a98a705377b8869a38a3b753070
Piter <1181720735@qq.com>
2019-11-07 17:34:47 +0800

    test

--- design-mode/src/main/java/创建型模式/简单工厂模式.java
+++ design-mode/src/main/java/创建型模式/简单工厂模式.java
@@ -11,7 +11,7 @@ package 创建型模式;
  * 创建一个工厂接口和创建多个工厂实现类，这样一旦需要增加新的功能，直接增加新的工厂类就可以了，
  * 不需要修改之前的代码
  */
-public class 简单工程模式 {
+public class 简单工厂模式 {
 
     public static void main(String[] args) {
         AnimalFactory animalFactory = new AnimalFactory();
